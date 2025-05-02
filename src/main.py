808079f84a54dee4fe54333bd2df2a1e409f84bd
sr-gus <lgmarin02@hotmail.com>
2025-05-01 19:20:53 -0600

    imp(main): added instructions

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
-import threading
 import time
-from input_handler import start_input_thread
 from visualization.renderer import Renderer
+from input_handler import start_input_thread
+
 
 def main():
     print("Bienvenido a 3D Computational Geometry")
-    
-    # Inicializar el renderizador
+    print("Escribe 'help' para ver la lista de comandos disponibles.")
+    print("Dentro de la ventana: presiona 'g' para alternar el grid, usa las flechas para mover el grid o cambiar su plano, y desplaza la rueda del mouse para hacer zoom.")
+
+    # Inicializar el renderizador y ventana
     renderer = Renderer()
     renderer.initialize_window()
 
-    # Iniciar el hilo para procesar comandos desde la consola
+    # Iniciar hilo de comandos desde la consola
     start_input_thread(renderer)
 
     # Bucle principal de renderizado
-    while True:
-        renderer.update_scene()
-        time.sleep(0.01)
+    try:
+        while True:
+            renderer.update_scene()
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        print("Saliendo...")
+
 
 if __name__ == '__main__':
     main()
